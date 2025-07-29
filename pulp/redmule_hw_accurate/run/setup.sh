c8473a494b5341c109cfdd6725803d46700795d2
Davide Nadalini <d.nadalini@unibo.it>
2025-07-29 11:51:07 +0200

    Remove unwanted files and partially fix RedMule integration

--- pulp/redmule_hw_accurate/run/setup.sh
+++
@@ -1,6 +0,0 @@
-#To avoid passing the paths as an argument every time make is run, set your paths here and source this script to setup the global variables used in the Makefile
-export PULP_RISCV_GCC_TOOLCHAIN=/home/pulpbox/pulp-sdk
-export GVSOC_PATH=/home/pulpbox/Work        # Needs this folder as a submodule as the "pulp/" folder
-export REDMULE_PATH=/home/pulpbox/Work/dnadalini_gvsoc/redmule-belano/models/pulp/redmule
-export PULP_SDK_PATH=/home/pulpbox/pulp-sdk
-export TEST_PATH=/home/pulpbox/Work/dnadalini_gvsoc/redmule-belano/models/pulp/redmule/test
