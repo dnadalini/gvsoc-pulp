3f9a4e5a3cb3ccf48ed90226d0d397cc4feba251
Davide Nadalini <d.nadalini@unibo.it>
2025-07-29 11:51:07 +0200

    Remove unwanted files and partially fix RedMule integration

--- pulp/redmule_hw_accurate/redmule.py
+++
@@ -1,9 +0,0 @@
-import gsystree as st
-
-class RedMule(st.Component):
-
-    def __init__(self, parent, name):
-
-        super(RedMule, self).__init__(parent, name)
-
-        self.set_component('pulp.redmule.redmule')
